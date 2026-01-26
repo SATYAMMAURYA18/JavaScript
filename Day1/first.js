8c044302f0a44c7bc5fd786d84d5d66c588a97ed
SATYAM MAURYA <satyammaurya24363@gmail.com>
2026-01-26 18:24:32 +0530

    Day1

---
+++ Day1/first.js
@@ -0,0 +1,14 @@
+console.log("Hello,Satyam");
+let num=10;
+console.log(num);
+console.log(num*4);
+
+let name="Satyam";
+console.log(name);
+console.log(name+4);
+
+let b=5;
+console.log(num+b);
+
+let t=3.4;
+console.log(t)
