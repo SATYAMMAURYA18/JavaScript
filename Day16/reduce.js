f6d8c476f3ee43c6cd8447397d6777a26a3f1dea
SATYAM MAURYA <satyammaurya24363@gmail.com>
2026-02-16 19:23:14 +0530

    Real world use of Reduce

--- Day16/reduce.js
+++ Day16/reduce.js
@@ -11,3 +11,18 @@ let result=a.reduce((accumalator,currentValue)=>{
     return accumalator;
 },0)//here intially accumalator me 0 store hai.
 console.log(result);
+
+//real world problem if an array come from backend i have to various number of product present in the array and return it in the form of object
+//there fore we use reduce to minimize the logic
+let arr=["apple","orange","banana","guava","papaya","apple","orange","Lichi","banana","guava","papaya","Mango","apple","orange","banana","guava","papaya","Lichi"];
+const result1=arr.reduce((fruit,current)=>{
+    if(fruit.hasOwnProperty(current))
+    {
+        fruit[current]++;
+    }
+    else{
+        fruit[current]=1;
+    }
+    return fruit;
+}, {});
+console.log(result1);
