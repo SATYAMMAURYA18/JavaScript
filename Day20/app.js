68a2920fce29e1157d4106a7b087279f2399f81d
SATYAM MAURYA <satyammaurya24363@gmail.com>
2026-02-20 19:33:53 +0530

    Olympic LA 28

---
+++ Day20/app.js
@@ -0,0 +1,24 @@
+const targetDate = new Date("July 14, 2028 20:00:00").getTime();
+//console.log(targetDate);
+const updateCountdown=()=>{
+    const now=new Date().getTime();
+    const distance=targetDate-now;
+
+    const days=Math.floor(distance/(1000*60*60*24));
+    const hours=Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+    const minutes=Math.floor((distance%(1000*60*60))/(1000*60));
+    const seconds=Math.floor((distance%(1000*60))/1000);
+
+    document.getElementById("days").innerText=days.toString().padStart(2,'0');
+    document.getElementById("hours").innerText = hours.toString().padStart(2,'0');
+    document.getElementById("minutes").innerText = minutes.toString().padStart(2, '0');
+    document.getElementById("seconds").innerText = seconds.toString().padStart(2, '0');
+    if(distance<0)
+    {
+        clearInterval(timeInterval);
+        document.querySelector(".countdown-card").innerHTML="<h1>LET THE GAMES BEGIN!</h1>";
+    }
+}
+
+const timeInterval=setInterval(updateCountdown,1000);
+updateCountdown();
